ea0d90073b7792adb18918f50069ba0f4a7a14d1
Mhiho <m.pelka7@gmail.com>
2018-05-06 09:26:32 +0200

    BurgerReady

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
+
+
+const orderSummary = (props) => {
+  const ingredientsSummary = Object.keys(props.ingredients)
+      .map(igKey => {
+        return ( <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]} </li> );
+      })
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicius burger with the following ingredients:</p>
+      <ul>
+        {ingredientsSummary}
+      </ul>
+      <p>Continue for checkout?</p>
+      <Button btnType="Danger" clicked={props.clickedCancel}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.clickedContinue}>CONTINUE</Button>
+    </Aux>
+  )
+};
+
+export default orderSummary;
