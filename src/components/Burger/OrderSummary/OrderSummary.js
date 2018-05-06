b25eee6c9380bcf6713e47aac65230e0a69d102e
Mhiho <m.pelka7@gmail.com>
2018-05-06 11:31:13 +0200

    SideDrawer

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientsSummary}
       </ul>
+      <p> <strong>Total price: {props.price.toFixed(2)}</strong> </p>
       <p>Continue for checkout?</p>
       <Button btnType="Danger" clicked={props.clickedCancel}>CANCEL</Button>
       <Button btnType="Success" clicked={props.clickedContinue}>CONTINUE</Button>
